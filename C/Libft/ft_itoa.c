e965087495e3d6d6ad3d1f92757a9e6be34eb3f7
matezzi777 <matezzi777@gmail.com>
2024-01-03 15:46:50 +0100

    ft_itoa() added

--- C/Libft/ft_itoa.c
+++ C/Libft/ft_itoa.c
@@ -5,62 +5,71 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/11/11 00:48:38 by marvin            #+#    #+#             */
-/*   Updated: 2023/11/11 00:48:38 by marvin           ###   ########.fr       */
+/*   Created: 2023/11/06 16:34:13 by marvin            #+#    #+#             */
+/*   Updated: 2024/01/03 15:46:14 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+#include <stdlib.h>
 
-int	ft_atoi(char	*str)
+char	*ft_itoa(int n);
+
+//OK
+int		ft_strlen(char *str)
 {
-	int	nombre;
 	int	i;
 
-	nombre = 0;
 	i = 0;
-	while (str[i] >= '0' && str[i] <= '9')
-	{
-		nombre = nombre * 10;
-		nombre = nombre + str[i] - '0';
+	while (str[i])
 		i++;
-	}
-	if (str[0] == '-')
-	return (nombre);
+	return (i);
 }
 
-int	size_needed(int n)
+//OK
+int		absolute(int n)
 {
-	// exemples :  100 -> 3 + 1 = 4
-	// 			54321 -> 5 + 1 = 6
-	// 			-54321 - > 5 + 2 = 7
-	int	size;
-
-	size = 1;
-	if (n < 0)
-		size++;
-	while (n % 10 != 0)
-	{
-		n = n / 10;
-		size++;
-	}
-	return (size);
-}
-
-// char	*ft_itoa(int n)
-// {
-// 	int	len;
-
-// 	// 1. Récupérer la taille de la chaine à convertir (nbr chiffres de n +1 (+2 si nombre négatif))
-// 	len = size_needed(n);
-// }
-
-#include <stdio.h>
-
-int	main(int argc, char **argv)
-{
-	if (argc == 2)
-		printf("Nombre : %s --- Taille nécessaire : %d\n\n", argv[1], size_needed(ft_atoi(argv[1])));
+	if (n >= 0)
+		return (n);
 	else
-		printf("Erreur : Nombre d'arguments invalides.\n");
+		return (-n);
+}
+
+//OK (inclus le \0)
+int		size_to_malloc(int n)
+{
+	int	i;
+
+	i = 2;
+	if (n < 0)
+		i++;
+	while (n / 10 != 0)
+	{
+		i++;
+		n = n / 10;
+	}
+	return (i);
+}
+
+//OK
+char	*ft_itoa(int n)
+{
+	int		i;
+	int		len;
+	char	*result;
+
+	len = size_to_malloc(n);
+	i = len - 2;
+	result = (char *)malloc(len * sizeof(char));
+	if (n == 0)
+		result[0] = '0';
+	if (n < 0)
+		result[0] = '-';
+	while (n != 0)
+	{
+		result[i] = absolute(n % 10) + '0';
+		i--;
+		n = n / 10;
+	}
+	result[len - 1] = '\0';
+	return (result);
 }
